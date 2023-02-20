87b8f7a897fbc10ae21aa22a66e2482c33549bbf
yangzol <87360723+yangzol@users.noreply.github.com>
2023-02-20 10:27:15 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://yydsys.top/duo/v.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://www.vvxxz.com/live/m.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
